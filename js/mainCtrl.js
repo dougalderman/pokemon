f876999ec28ce44c0c285c3a5bd2c725e6b92db4
Doug Alderman <agnadcp-d1@yahoo.com>
2016-05-09 10:13:35 -0600

    5/9/16 changes

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -4,8 +4,15 @@ angular.module('pokemon')
   $scope.getThePokemon = function(num) {
       pokemonSvc.getPokemon(num).then(function(data) {
       $scope.pokemon = data;
-      console.log(data);
-    })
+      console.log('data', data);
+    });
+  };
+ 
+  $scope.getLotsOfPokemons = function(numOfPokemons) {
+	  pokemonSvc.getLotsOfPokemon(numOfPokemons).then(function(data) {
+    	$scope.allPokemon= data.pokeData;
+		console.log('data', data)
+	  });
   };
 
 });
--- js/pokemonSvc.js
+++ js/pokemonSvc.js
@@ -1,5 +1,32 @@
 angular.module('pokemon')
-.service('pokemonSvc', function($http) {
+.service('pokemonSvc', function($http, $q) {
+
+  this.getLotsOfPokemon = function(count) {
+    var dfd = $q.defer();
+
+    var pokemonData = [];
+   	  
+	function checkForAllDataReceived() {
+		if (pokemonData.length === count) {
+			dfd.resolve({
+				pokeData: pokemonData
+			});
+		}
+	}
+
+    for (var i = 1; i <= count; i++) {
+      $http({
+        method: 'GET',
+        url: 'http://pokeapi.co/api/v1/pokemon/' + i
+      }).then(function(response) {
+        if(response.status === 200) {
+          pokemonData.push(response.data);
+        }
+		checkForAllDataReceived(); 
+      })
+    }
+	return dfd.promise;  
+  };
 
   this.getPokemon = function(num) {
     return $http({
